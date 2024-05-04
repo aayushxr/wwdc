c431b3bfcf6095ec8afe24b620e1c0be8a71865b
Aayush Rajagopalan <61064137+Aayush-Rajagopalan@users.noreply.github.com>
2024-05-04 22:16:38 +0300

    donate

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-import { withVercelToolbar } from "@vercel/toolbar/plugins/next";
 const nextConfig = {
   images: {
     domains: ["cdn.aayus.me"],
   },
 };
 
-export default withVercelToolbar(nextConfig);
+export default nextConfig;
