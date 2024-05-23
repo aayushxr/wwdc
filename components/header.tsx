14fe2240ed1c0f252b91aee949cfcc359192b18e
Aayush Rajagopalan <61064137+Aayush-Rajagopalan@users.noreply.github.com>
2024-05-23 21:30:20 +0300

    heading fix

--- components/header.tsx
+++ components/header.tsx
@@ -12,7 +12,7 @@ export default function Page({
       <div className="container h-full flex">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-zinc-300 [mask-image:radial-gradient(ellipse_at_center,transparent_30%,white)]"></div>
         <div className="flex items-start justify-center z-20  max-w-6xl">
-          <div className=" pt-20 ">
+          <div className=" pt-20 flex flex-col items-center ">
             <h1 className="text-4xl max-w-6xl relative z-50 text-center font-bold leading-snug tracking-tight text-zinc-900 lg:text-4xl lg:leading-tight xl:text-5xl xl:leading-tight dark:text-white">
               {title}
             </h1>
